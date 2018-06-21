2d580b564cd28d831f4a23157f18cb9aa69cc797
m305-7\w401 <A103082022@mail.shu.edu.tw>
2018-06-21 16:13:27 +0800

    版面100分!!

---
+++ todoList/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todoList
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_MouseDown(object sender, RoutedEventArgs e)
+        {
+            // create a new rectangle
+            TodoItem Item = new TodoItem();
+
+            // put it to Todostack
+            TodoItemList.Children.Add(Item) ;
+        }
+    
+    }
+}
---
+++ todoList/TodoItem.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todoList
+{
+    /// <summary>
+    /// TodoItem.xaml 的互動邏輯
+    /// </summary>
+    public partial class TodoItem : UserControl
+    {
+        public TodoItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Price_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Price.Text = "";
+        }
+
+        private void Date_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Date.Text = "";
+        }
+
+        private void Item_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //----//
+        }
+
+       
+
+    }
+}
